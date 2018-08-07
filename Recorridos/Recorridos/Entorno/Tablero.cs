3a16ab7e1b965a3912b6311d4a7de238d8078bcb
Juan Antonio Bueno <juan.bueno@melia.com>
2018-08-07 16:55:44 +0200

    Inicio A*

--- Recorridos/Recorridos/Entorno/Tablero.cs
+++ Recorridos/Recorridos/Entorno/Tablero.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace Recorridos.Recorridos.Entorno {
     class Tablero {
-        private int ancho, largo;
         public enum Estado {libre, ocupado, visitado, origen, destino};
 
         private Estado[,] casillas;
@@ -14,8 +13,13 @@ namespace Recorridos.Recorridos.Entorno {
         // Nodo de origen y de destino
         private int[] origen;
         private int[] destino;
+        private int ancho;
+        private int alto;
 
         public Tablero(int ancho, int alto) {
+            this.ancho = ancho;
+            this.alto = alto;
+
             casillas = new Estado[ancho, alto];
             
             for (int x = 0; x < ancho; x++) {
@@ -38,9 +42,9 @@ namespace Recorridos.Recorridos.Entorno {
             set { this.ancho = value; }
         }
 
-        public int Largo {
-            get { return largo; }
-            set { this.largo = value; }
+        public int Alto {
+            get { return alto; }
+            set { this.alto = value; }
         }
 
         public int[] Origen {
@@ -51,6 +55,39 @@ namespace Recorridos.Recorridos.Entorno {
         public int[] Destino {
             get { return destino; }
             set { destino = value; }
+        }
+
+        /// <summary>
+        /// Transforma la posición x, y en un ID secuencial
+        /// </summary>
+        public int GetId(int x, int y) {
+            return x + y * ancho;
+        }
+
+        public int GetXY(int id) {
+            int x = id % ancho;
+            int y =(int)(id / ancho);
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Devuelve la lista de casillas adyacentes (como máximo devuelve 8).
+        /// </summary>
+        /// <returns></returns>
+        public List<int> Vecinos(int x, int y) {
+            List<int> adyacentes = new List<int>();
+
+            for (int i = x-1; i <= x + 1; i++) {
+                if (i >= 0 && i < ancho) {
+                    for (int j = y - 1; j <= y + 1; j++) {
+                        if (j >= 0 && j < ancho && x != i && y != j) 
+                            adyacentes.Add(GetId(i, j));                        
+                    }
+                }
+            }
+
+            return adyacentes;
         }
 
 
